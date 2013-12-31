e6562d667289e27da261e7203785c6483f5ff052
Volker Krebs <volker.krebs@abas.de>
2013-12-31 11:58:44 +0100

    Apply the plugin itself on the loaded content

--- jquery.mobile.lazylist.js
+++ jquery.mobile.lazylist.js
@@ -13,16 +13,7 @@
     },
     _create: function() {
         console.log('_create');
-        //TODO: Better selection
-        moreButton = $(".lazylist-morebtn");
-        //TODO: Handling when no more button was found
-        //console.log(moreButton);
-        moreButton.bind("click", $.proxy(function(event) {
-            //console.log(event);
-            this._loadMore(event.target.href);
-            //abbort and load with ajax
-            return false;
-        }, this));
+        this.apply();
         //apply listview plugin
 	this.element.listview();
     },
@@ -37,7 +28,6 @@
                 console.log('done loading');
                 //console.log(html);
                 //console.log(textStatus);
-                console.log(widget.element);
                 //console.log(jqXHR);
                 var content = $( html.trim() );
                 widget.element.append(content);
@@ -47,8 +37,10 @@
                     var pluginName = content.attr('data-role');
                     content[pluginName]();
                 }
+                //refresh needed for the just loaded content
                 widget.element.listview('refresh');
-                //widget.element.trigger('create');
+                //apply this widget (more button and stuff)
+                widget.apply();
             },
             error: function(jqXHR, textStatus, errorThrown ) {
                 console.log('error loading');
@@ -68,15 +60,26 @@
     },
     _removeMoreButton: function() {
         console.log('removing more button');
-        console.log(moreButton);
+        //console.log(moreButton);
         //TODO: more button genau finden um ihn komplett zu entfernen (samt evtl. Elternelemente)
         moreButton.parent().remove();
+    },
+    apply: function() {
+        console.log('apply');
+        //TODO: Better selection
+        moreButton = $(".lazylist-morebtn");
+        //TODO: Handling when no more button was found
+        //console.log(moreButton);
+        moreButton.bind("click", $.proxy(function(event) {
+            //console.log(event);
+            this._loadMore(event.target.href);
+            //abbort and load with ajax
+            return false;
+        }, this));
     }
     });
-
     //auto self-init widgets
     $(document).bind('pagecreate', function(event) {
 	$($.mobile.lazylist.prototype.options.initSelector, event.target).lazylist();
     });
-
 }(jQuery));
